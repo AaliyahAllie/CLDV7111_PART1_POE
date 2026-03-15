8c1864ce51e4abea0586d6fec4e0027e49299a51
Aaliyah <ST10212542@RCCONNECT.EDU.ZA>
2026-03-15 16:16:16 +0200

    Updated shared layout and program.cs

--- CLDV7111_PART1/Program.cs
+++ CLDV7111_PART1/Program.cs
@@ -1,7 +1,14 @@
+using CLDV7111_PART1.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register EF Core DbContext with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -9,7 +16,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
     app.UseHsts();
 }
 
